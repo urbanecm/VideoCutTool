5c04502d9ffe8b699df56c4422531eed11843dba
gopavasanth <gopavasanth1999@gmail.com>
2019-03-20 21:08:40 +0530

    Redirected Procfile to use index.js

--- src/App.js
+++ src/App.js
@@ -24,7 +24,6 @@ class App extends Component {
                 <Player playsInline poster="/assets/poster.png"
                   src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
                 />
-              
                 </div>
               </Col>
               <Col span={8}>
